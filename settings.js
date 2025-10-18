d4d1416608748cfe725e5de7896c03974c9cc201
Jithuzz5 <manujithu12@gmail.com>
2025-10-18 23:40:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Jithuzz𓆩ꨄ︎𓆪 Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Jithuzz𓆩ꨄ︎𓆪 Bot",
+  botOwner: 'Jithuzz𓆩ꨄ︎𓆪', // Your name
+  ownerNumber: '919562435568', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
